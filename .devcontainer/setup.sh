35ebe321bd42d47c6e852865b0dd4a0bcca0fb84
lppnes <lppnes@users.noreply.github.com>
2026-03-17 14:51:15 +0000

    Add devcontainer for SN22 testnet miner (Codespaces)

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+echo "=== SN22 Testnet Miner Setup ==="
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -e .
+pip install bittensor-cli
+npm install -g pm2
+echo "Setup complete!"
